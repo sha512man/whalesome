cf7bfb62c1dc38de33449025a54b9c8296a3335d
Michal Odcházel <michal.odchazel@unnits.com>
2025-04-22 16:45:51 +0200

    chore: Edit creating user (work with rootless nginx and php-fpm)

--- src/init.d/001-create_user.sh
+++ src/init.d/001-create_user.sh
@@ -1,28 +1,39 @@
 #!/bin/sh
 
-if [ ! -z "${USER}" ] ; then
-    echo "User specified"
+set -e
 
-    userName=$(echo ${USER} | cut -d':' -f1)
-    userId=$(echo ${USER} | cut -d':' -f2)
+if [ -n "${USER}" ] ; then
+    log "INFO: User specified" 1
+
+    userName=$(echo "${USER}" | cut -d':' -f1)
+    userId=$(echo "${USER}" | cut -d':' -f2)
 
     if id "${userName}" >/dev/null 2>&1; then
-	    echo "Found user ${userName}, deleting..."
-      deluser "${userName}"
+	      log "INFO: Found user ${userName}, deleting..." 2
+        deluser "${userName}"
     fi
 
     if grep -q -E "^${userName}:" /etc/group; then
-      echo "Found group ${userName}, deleting..."
-      delgroup "${userName}"
+        log "INFO: Group ${userName} exists." 2
+    else
+        log "INFO: Group ${userName} not exists, creating..." 2
+        addgroup --gid "${userId}" "${userName}"
     fi
 
-    echo "Creating new user ${userName}"
+    log "INFO: Creating new user ${userName}" 2
+
+    rm -rf "/home/${userName}"
 
     adduser \
         --uid "${userId}" \
         --disabled-password \
+        --ingroup "${userName}" \
+        --home "/home/${userName}" \
         --gecos "" \
-    "${userName}"
+        "${userName}"
 
-    chown -R "${userName}:${userName}" "/home/${userName}"
+    if [ "${INIT_DEBUG}" = "3" ]; then
+        cat /etc/passwd | grep "${userName}"
+        cat /etc/group | grep "${userName}"
+    fi
 fi
