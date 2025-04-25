761808a8dee9b7efaafe33e4782d3265d96a4c5f
Michal Odcházel <michal.odchazel@unnits.com>
2025-04-25 11:44:18 +0200

    feat: Create a script for a print message for concrete level verbose

--- src/packages/php/fpm/init.d/102-fpm-copy_config_files.sh
+++ src/packages/php/fpm/init.d/102-fpm-copy_config_files.sh
@@ -7,8 +7,6 @@ set -e
 PHP_INI_FILE_ENV="${PHP_INI_DIR}/php.ini-${FPM_PHP_INI_ENV}"
 
 if [ -f "${PHP_INI_FILE_ENV}" ]; then
-    if [ "${INIT_DEBUG}" -ge "2" ]; then
-        echodt "INFO: copy ${PHP_INI_FILE_ENV} into ${PHP_INI_DIR}"
-    fi
+    log "INFO: copy ${PHP_INI_FILE_ENV} into ${PHP_INI_DIR}" 2
     cp "${PHP_INI_FILE_ENV}" "${PHP_INI_DIR}/php.ini"
 fi
