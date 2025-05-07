9a9e29f7dafe11d698ddd5ae6af6c92602d5f845
Michal Odcházel <michal.odchazel@unnits.com>
2025-05-07 13:02:47 +0200

    chore: Update xdebug a latest version

--- src/packages/php/xdebug/install.sh
+++ src/packages/php/xdebug/install.sh
@@ -8,4 +8,4 @@ container_init_directory="${1}"
 echo "Copying xdebug initial scripts to the global init directory"
 cp -R "${current_dir}/init.d/." "${container_init_directory}"
 
-install-php-extensions xdebug-3.3.1
+install-php-extensions xdebug-3.4.2
