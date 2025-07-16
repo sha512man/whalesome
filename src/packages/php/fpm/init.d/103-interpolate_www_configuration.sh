30c711e896beae9e815b9e3237217700ccaa2962
Michal Odcházel <michal.odchazel@unnits.com>
2025-07-16 12:09:28 +0200

    feat: Added option to set `ping.path`, `pm.status_path` and `pm.status_listen` for php-fpm

--- src/packages/php/fpm/init.d/103-interpolate_www_configuration.sh
+++ src/packages/php/fpm/init.d/103-interpolate_www_configuration.sh
@@ -9,6 +9,9 @@ tpl /opt/whalesome/packages/php/fpm/www.conf.tpl \
   fpmRuntimeUser="${WWW_USER}" \
   fpmRuntimeGroup="${WWW_GROUP}" \
   fpmPort="${FPM_PORT}" \
+  fpmPingPath="${FPM_PING_PATH}" \
+  fpmStatusPath="${FPM_STATUS_PATH}" \
+  fpmStatusListenPort="${FPM_STATUS_LISTEN_PORT}" \
   fpmPmMaxChildren="${FPM_PM_MAX_CHILDREN}" \
   fpmPmStartServers="${FPM_PM_START_SERVRES}" \
   fpmPmMinSpareServers="${FPM_PM_MIN_SPARE_SERVRES}" \
