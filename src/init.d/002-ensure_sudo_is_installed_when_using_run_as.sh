9ed1475e23317909e15e87a3ee8167145ec9a56a
Dan Charousek <dan.charousek@unnits.com>
2026-02-28 22:02:02 +0100

    feat: Add possibility to start RUN_AS via su-exec instead the sudo command

--- src/init.d/002-ensure_sudo_is_installed_when_using_run_as.sh
+++ src/init.d/002-ensure_sudo_is_installed_when_using_run_as.sh
@@ -3,8 +3,15 @@
 set -e
 
 if [ -n "${RUN_AS}" ] ; then
+  if [ -n "${USE_SU_EXEC}" ] && [ "${USE_SU_EXEC}" = "1" ]; then
     if ! command -v su-exec &> /dev/null; then
-        echodt "ERROR: su-exec package is not installed, please install 'pkg su-exec'"
+        echodt "ERROR: su-exec package not found. Install it via 'pkg su-exec'"
         exit 1
     fi
+  else
+    if ! command -v sudo &> /dev/null; then
+        echodt "ERROR: sudo package not found. Install it via 'pkg sudo'"
+        exit 1
+    fi
+  fi
 fi
